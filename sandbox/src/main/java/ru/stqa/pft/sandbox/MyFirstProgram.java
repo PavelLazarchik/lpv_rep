990227a38591f620f8341edf3294aa920118e5ae
PavelLazarchik <pavel_lazarchik@tut.by>
2017-04-14 00:41:54 +0300

    added some new methods

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,9 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main (String[] args) {
-        System.out.println("Hello, world!");
-        int i = 6;
-        int s = i * i;
-        System.out.println("Площадь квадрата со стороной " + i + " = " + s);
+        hello("world");
+        hello("User");
+        hello("Pavel");
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Плащадь прямоугольника со сторонами " + a + " и " + b + " = " + areaRectangle(a,b));
+
+        System.out.println("Пладщадь квалрата со сторонами " + a + " равна " + areaSquare(a));
+    }
+
+    public static void hello (String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double areaRectangle (double a, double b) {
+       return a * b;
+    }
+
+    public static double areaSquare (double a) {
+        return a * a;
     }
 }
