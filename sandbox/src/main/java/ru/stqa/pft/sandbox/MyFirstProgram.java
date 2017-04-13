739bbe67fd4a0cce2bb12c01d15b8909c9c3f062
PavelLazarchik <pavel_lazarchik@tut.by>
2017-04-13 23:27:44 +0300

    added some procedures with variables

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,5 +3,8 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main (String[] args) {
         System.out.println("Hello, world!");
+        int i = 6;
+        int s = i * i;
+        System.out.println("Площадь квадрата со стороной " + i + " = " + s);
     }
 }
